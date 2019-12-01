ab913a15b06394301133900fcff3b346e5e963ae
shuheydev <shuhey.dev@gmail.com>
2019-12-01 12:50:39 +0900

    Add project files.

---
+++ XFRefreshViewPractice/XFRefreshViewPractice/Models/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace XFRefreshViewPractice.Models
+{
+    public class Person: INotifyPropertyChanged
+    {
+        private string _firstName;
+        public string FirstName {
+            get => _firstName;
+            set
+            {
+                if(_firstName==value)
+                {
+                    return;
+                }
+                _firstName = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(FullName));
+                RaisePropertyChanged(nameof(AvatarUrl));
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (_lastName == value)
+                {
+                    return;
+                }
+                _lastName = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(FullName));
+                RaisePropertyChanged(nameof(AvatarUrl));
+            }
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string AvatarUrl => $"https://robohash.org/{LastName}-{FirstName}?set=set4";
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ XFRefreshViewPractice/XFRefreshViewPractice/Views/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XFRefreshViewPractice.Models;
+
+namespace XFRefreshViewPractice.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage,INotifyPropertyChanged
+    {
+        private List<Person> _people;
+        public List<Person> People
+        {
+            get => _people;
+            set
+            {
+                _people = value;
+                OnPropertyChanged(nameof(People));
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            RefreshCommand = new Command(ExecuteRefreshCommand);
+
+            People = GeneratePeople();
+
+            this.BindingContext = this;
+        }
+
+        private List<Person> GeneratePeople(string additional="")
+        {
+            var people = new List<Person>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                people.Add(new Person
+                {
+                    FirstName = $"FirstName {i}",
+                    LastName = $"LastName {i} {additional}",
+                });
+            }
+
+            return people;
+        }
+
+        private void ExecuteRefreshCommand(object obj)
+        {
+            People.Clear();
+
+            People = GeneratePeople(DateTime.Now.ToString());
+
+            //ぐるぐるを非表示にする
+            IsRefreshing = false;
+        }
+
+        public ICommand RefreshCommand { get; }
+
+        private bool _isRefreshing;
+        public bool IsRefreshing
+        {
+            get => _isRefreshing;
+            set
+            {
+                _isRefreshing = value;
+                OnPropertyChanged(nameof(IsRefreshing));
+            }
+        }
+
+    }
+}
